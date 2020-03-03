3634ce375c3d4a4bdffdb3316eca3c57da5b104a
Alex <alexyoung1324@gmail.com>
2020-03-03 19:52:25 +1100

    initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("Enter a number: ");                              //k is an integer
+    scanf("%f", &x);
+    y = (x + 2)/(x - 1);                   //input a number
+    printf("y: %f\n", y);     // %d formts an integer
+    return 0;
+}
